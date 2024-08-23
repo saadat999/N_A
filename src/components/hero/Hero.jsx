d8bde3f6f6b464c5385d86f23af1cfd51805b7d6
saadat <devilwq99@gmail.com>
2024-08-23 18:55:16 +0600

    update images 6

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,19 +2,16 @@
 
 import React from 'react'
 import './Hero.css'
-// import im1 from '../../assets/kol.png'
-// import im2 from '../../assets/ezhe.jpg'
-// import im3 from '../../assets/first.jpg'
+
 
 const Hero = () => {
     return (
         <div className='container'>
             <div className='hero'>
                 <img width={300} height={350} src={'/kol.png'} alt="" />
-                <img width={300} height={350} src={'./kol.png'} alt="" />
-                {/* <img width={300} height={350} src={im1} alt="" />
-                <img width={300} height={350} src={im2} alt="" />
-                <img width={300} height={350} src={im3} alt="" /> */}
+                <img width={300} height={350} src={'/ezhe.jpg'} alt="" />
+                <img width={300} height={350} src={'/first.jpg'} alt="" />
+
             </div>
         </div>
     )
